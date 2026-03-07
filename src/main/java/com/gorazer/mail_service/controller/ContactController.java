8c4646e6688f77210848093517f7097835bcd5be
Junior JGU <gomezjunior549@gmail.com>
2026-03-07 20:44:07 -0300

    V

--- src/main/java/com/gorazer/mail_service/controller/ContactController.java
+++ src/main/java/com/gorazer/mail_service/controller/ContactController.java
@@ -7,14 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @RestController
-@RequestMapping("/api/contact")
-@CrossOrigin(origins = "https://juniorjgu.dev/")
+@RequestMapping("/api")
+@CrossOrigin(origins = "https://juniorjgu.dev")
 public class ContactController {
 	
 	@Autowired
     private EmailService emailService;
 	
-    @PostMapping
+    @PostMapping("/contact")
     public ResponseEntity<?> sendEmail(@Valid @RequestBody ContactRequest request) {
         
 
